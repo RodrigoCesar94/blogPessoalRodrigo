69d2d73e9f99104bc7be2b0d7ee18adf3f776540
Rodrigo Cesar <rodrigocesarrcf.94@gmail.com>
2022-01-24 22:33:23 -0300

    adicionado componente de inicio

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -17,5 +18,14 @@ export class AuthService {
   }
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('https://blogpessoalrodrigocesar.herokuapp.com/usuarios/cadastrar', user)
+  }
+  //se estiver dentr da função, declarar variável  com let.
+  logado(){
+  let ok: boolean = false;
+
+  if(environment.token != ''){
+    ok = true
+  }
+  return ok
   }
 }
